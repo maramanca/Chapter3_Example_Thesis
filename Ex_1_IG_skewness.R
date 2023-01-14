7115ab84b7fb6754c2fa133032bb671245793296
Mara Manca <98509965+maramanca@users.noreply.github.com>
2023-01-14 06:26:44 +0100

    Add files via upload

---
+++ Ex_1_IG_skewness.R
@@ -0,0 +1,222 @@
+#### IG skewness example ####
+library(mvtnorm)
+
+
+load("Code_IG-SIS.RData")
+
+
+#### Data input of the two populations and setting the population parameters ####
+
+x1 <- c(1460, 4050, 3570, 2060, 1300, 1390, 1720, 6280, 1360, 7440)
+
+x2 <- c(5320, 1400, 3240, 2710, 4520, 4840, 8320, 13900, 71500, 6250)
+
+sk1 <- 3*(sd(x1)/mean(x1))
+sk2 <- 3*(sd(x2)/mean(x2))
+  
+n1 <- length(x1)
+n2 <- length(x2)
+
+t1 <- sum(x1)
+t2 <- sum(x2)
+
+m1 <- sum(1/x1)
+m2 <- sum(1/x2)
+
+# Inverse Gaussian log-posterior distribution with parameter theta = (mu, lambda)
+log_posterior_IG <- function(theta, n, t, m){
+  
+  pd.1 <- ((n + 1)/2)*log(theta[2])
+  pd.2 <- (-1/2)*log(theta[1])
+  pd.3 <- (-t/2)*(theta[2]/(theta[1]^2))
+  pd.4 <- n*(theta[2]/theta[1])
+  pd.5 <- -1/2*m*theta[2]
+  
+  out = pd.1 + pd.2 + pd.3 + pd.4 + pd.5
+  
+  return(out)
+}
+
+
+
+#### Metropolis-Hastings algorithm for the Inverse Gaussian ####
+
+set.seed(5)
+MH_IG <- function(S, B, theta0, n, t, m, tau, thin){
+  
+  R <- B + S
+  mat <- matrix(NA, nrow = R, ncol = length(theta0))
+  acc <- rep(0, R)
+  
+  theta_chain <- theta0
+  
+  for (i in 1:R) {
+    # Sampling from the proposal
+    l_theta_star <- rmvnorm(1, mean = log(theta_chain), sigma = tau)
+    theta_star <- exp(l_theta_star)
+    
+    # Computation of r
+    l1 <- log_posterior_IG(theta = theta_star, n = n, t = t, m = m)
+    l2 <- log_posterior_IG(theta = theta_chain, n = n, t = t, m = m)
+    r <- min(1, exp(l1 - l2))
+    
+    # Drawing U from the uniform distribution in (0,1)
+    u <- runif(1)
+    
+    # Accepting or rejecting theta_star
+    if(r >= u) {
+      theta_chain <- theta_star
+      acc[i] <- 1
+    }
+    mat[i,] <- theta_chain
+  }
+  
+  # Thinning
+  m1 <- mat[(B+1):R,]
+  m2 <- m1[seq(1, nrow(m1), by = thin),]
+  acc1 <- acc[(B + 1):R]
+  acc2 <- acc1[seq(1, length(acc1), by = thin)]
+  
+  # Output from thinning and discarding burn-in values
+  list(values = m2, acc_rate = sum(acc2)/length(acc2))
+}
+
+
+
+#### Application of the sampling algorithm to Population 1 ####
+
+# theta_0 parameter initialization
+maximization_1 <- optim(function(x) -log_posterior_IG(x, n = n1, t = t1, m = m1),
+                       hessian = T, method = "BFGS", par = c(2800,6500))
+theta0_1 <- maximization_1$par
+theta0_1
+
+
+# tau covariance matrix for the proposal distribution
+# tau_1 <- 0.0000015*solve(maximization_1$hessian)
+# tau_1
+tau_1 <- matrix(c(1,1,1,1),2,2)
+
+# Sampling procedure
+S <- 600000 # number of samples
+B <- 300000  # burn-in samples
+
+set.seed(1)
+posterior_sample_1 <- MH_IG(S = S, B = B, theta0 = theta0_1, n = n1, t = t1,
+                        m = m1, tau = tau_1, thin = 5)
+
+mu_1 <- posterior_sample_1$values[, 1]
+lambda_1 <- posterior_sample_1$values[, 2]
+
+### Convergence diagnostics ###
+# Samples visualization, mu1 vs lambda1
+plot(posterior_sample_1$values, xlab = "mu_1", ylab = "lambda_1")
+
+# Acceptance rate
+posterior_sample_1$acc_rate
+
+# Traceplots
+par(mfrow = c(1,2))
+plot(mu_1, type = "l")
+plot(lambda_1, type = "l")
+
+# Istogrammi
+means <- c(mean(mu_1), mean(lambda_1))
+means
+medians <- c(median(mu_1), median(lambda_1))
+medians
+
+par(mfrow = c(1,2))
+{hist(mu_1, 3000, freq = F, xlab = "mu_1", main = "a) Histogram of mu_1")
+  abline(v = means[1], col = "red", lwd = 2)
+  abline(v = medians[1], col = "green", lwd = 2, lty = 3)}
+{hist(lambda_1, 3000, freq = F, xlab = "lambda_1", main = "b) lambda_1") 
+  abline(v = means[2], col = "red", lwd = 2)
+  abline(v = medians[2], col = "green", lwd = 2, lty = 3)}
+
+# ACF
+par(mfrow = c(1,2))
+acf(mu_1, main = "ACF for mu_1", lag.max = 10000)
+acf(lambda_1, main = "ACF for lambda_1", lag.max = 10000)
+
+
+
+#### Application of the sampling algorithm to Population 2 ####
+
+# theta_0 parameter initialization
+maximization_2 <- optim(function(x) -log_posterior_IG(x, n = n2, t = t2, m = m2),
+                       hessian = T, method = "L-BFGS-B", par = c(2800,6500))
+theta0_2 <- maximization_2$par
+
+# tau covariance matrix for the proposal distribution
+# tau_2 <- 0.0009*solve(maximization_2$hessian)
+tau_2 <- matrix(c(1,1,1,1),2,2)
+
+# Sampling procedure
+S <- 600000 # number of samples
+B <- 300000  # burn-in samples
+
+set.seed(1)
+posterior_sample_2 <- MH_IG(S = S, B = B, theta0 = theta0_2, 
+                        n = n2, t = t2,
+                        m = m2, tau = tau_2, thin = 5)
+
+mu_2 <- posterior_sample_2$values[, 1]
+lambda_2 <- posterior_sample_2$values[, 2]
+
+### Convergence diagnostics ###
+# Samples visualization, mu_2 vs lambda_2
+plot(posterior_sample_2$values, xlab = "mu_2", ylab = "lambda_2")
+
+# Acceptance rate
+posterior_sample_2$acc_rate
+
+# Traceplots
+par(mfrow = c(1,2))
+plot(mu_2, type = "l")
+plot(lambda_2, type = "l")
+
+# Istogrammi
+means <- c(mean(mu_2), mean(lambda_2))
+means
+medians <- c(median(mu_2), median(lambda_2))
+medians
+
+par(mfrow = c(1,2))
+{hist(mu_2, 3000, freq = F, xlab = "mu_2",main = "a) Histogram of mu_2")
+  abline(v = means[1], col = "red", lwd = 2)
+  abline(v = medians[1], col = "green", lwd = 2, lty = 3)}
+{hist(lambda_2, 3000, freq = F, xlab = "lambda_2", main = "b) lambda_2") 
+  abline(v = means[2], col = "red", lwd = 2)
+  abline(v = medians[2], col = "green", lwd = 2, lty = 3)}
+
+# ACF
+par(mfrow = c(1,2))
+acf(mu_2, main = "ACF for mu_2", lag.max = 1000)
+acf(lambda_2, main = "ACF for lambda_2", lag.max = 1000)
+
+
+
+#### BDM computation and hypothesis testing H: psi_1 = psi_2 ####
+
+N <- length(mu_1)
+
+psi_1 <- 3*sqrt(mu_1/lambda_1) 
+psi_2 <- 3*sqrt(mu_2/lambda_2)
+
+I <- sum(psi_1 < psi_2)/N
+I
+
+d_H <- function(Int){
+  
+  if(Int < 0.5){
+    out <- 1 - 2*I
+  }
+  else {
+    out <- 1 - 2*(1 - Int)
+  }
+}
+
+# BDM
+delta_H <- d_H(Int = I)
+delta_H
---
+++ Ex_3_Gamma_variances.R
@@ -0,0 +1,203 @@
+library(mvtnorm)
+library(coda)
+library(robustbase)
+
+# Comparison means Gamma
+Yield1 = c(0.001, 0.003, 0.007, 0.020,
+                              0.030, 0.040, 0.041, 0.077,
+                              0.100, 0.454, 0.490, 1.020)
+Yield2 = c(0.020, 0.031, 0.086, 0.130,
+                              0.160, 0.160, 0.180, 0.300,
+                              0.400, 0.440, 0.510, 0.720, 
+                              0.950)
+
+n1 <- length(Yield1)
+n2 <- length(Yield2)
+
+s1 <- sum(Yield1)
+s2 <- sum(Yield2)
+
+p1 <- prod(Yield1)
+p2 <- prod(Yield2)
+
+
+c(n1,s1,p1)
+c(n2,s2,p2)
+
+# Posterior
+posterior = function(theta,n,s,p){
+  out = (((exp(theta[2]))^(n*exp(theta[1])))/((gamma(exp(theta[1])))^n))*
+    p^(exp(theta[1])-1)*exp(theta[1]+theta[2]-s*exp(theta[2]))
+  
+  return(out)
+}
+
+
+# log-posterior
+log_posterior = function(theta,n,s,p){
+  out = exp(theta[1])*n*theta[2]-n*log(gamma(exp(theta[1])))+
+    (exp(theta[1])-1)*log(p)+theta[1]+theta[2]-s*exp(theta[2])
+  return(out)
+}
+
+# Metropolis-Hastings
+MH = function(S, B, theta0, n, s, p, tau, thin){
+  
+  R = S+B
+  mat = matrix(NA, nrow = R, ncol = length(theta0))
+  acc = rep(0, R)
+  
+  Tau = matrix(0, length(tau), length(tau)); diag(Tau) = tau
+  
+  theta_chain = theta0
+  
+  for (m in 1:R) {
+    # Sampling from the proposal
+    theta_star = rmvnorm(1, mean = theta_chain, sigma = Tau)
+    # Compute r
+    r = min(1, exp(log_posterior(theta = theta_star, n = n, s = s, p = p)-
+                     log_posterior(theta = theta_chain, n = n, s = s, p = p)))
+    # Draw U from the uniform distribution in (0,1)
+    u = runif(1)
+    # Accept or reject theta.s
+    if(r >= u) {
+      theta_chain = theta_star
+      acc[m] = 1
+    }
+    mat[m,] = theta_chain
+  }
+  
+  m1 <- mat[(B+1):R,]
+  m2 <- mat[seq(1,nrow(m1),by=thin),]
+  acc1 <- acc[(B+1):R]
+  acc2 <- acc1[seq(1,length(acc1),by=thin)]
+  
+  # Output
+  list(values = m2, acc_rate=sum(acc2)/length(acc2))
+}
+
+#### Pop 1 ####
+
+# theta_0
+maximization_1 = optim(function(x) - log_posterior(x,n1,s1,p1),
+                       hessian=T, method = "L-BFGS-B", par=c(0,0))
+theta0_1 = maximization_1$par
+theta0_1
+
+# tau
+tau_1 = 1.5*diag(solve(maximization_1$hessian))
+
+# Simulation
+S = 150000
+B = 60000
+
+set.seed(1)
+posterior_sample_1 = MH(S = S, B = B, theta0 = theta0_1, n = n1, s = s1,
+                        p = p1, tau = tau_1, thin = 3)
+
+x11()
+plot(posterior_sample_1$values)
+
+# Accuracy rate
+posterior_sample_1$acc_rate
+
+# (alpha,beta)
+alpha_1  = exp(posterior_sample_1$values[,1])
+beta_1   = exp(posterior_sample_1$values[,2])
+
+# Traceplots
+par(mfrow=c(1,2))
+plot(alpha_1, type="l")
+plot(beta_1, type="l")
+
+# Histograms
+means=c(mean(alpha_1),mean(beta_1))
+means
+medians=c(median(alpha_1),median(beta_1))
+medians
+
+par(mfrow=c(1,2))
+{hist(alpha_1,3000, freq=F,xlab="a",main="a) Histogram of alpha_1")
+  abline(v=means[1], col="red",lwd=2)
+  abline(v=medians[1], col="green",lwd=2,lty=3)}
+{hist(beta_1,3000, freq=F,xlab="beta_1",main="b) beta_1") 
+  abline(v=means[2], col="red",lwd=2)
+  abline(v=medians[2], col="green",lwd=2,lty=3)}
+
+# ACF
+par(mfrow=c(1,2))
+acf(alpha_1,main="ACF for alpha_1",lag.max = 100)
+acf(beta_1,main="ACF for beta_1",lag.max = 100)
+
+# (mu,sigma2)
+mu_1     = alpha_1 / beta_1
+sigma2_1 = alpha_1 / beta_1^2
+
+####
+x11()
+plot(cumsum(mu_1) / seq_along(mu_1),type='l',col="red")
+plot(cumsum(sigma2_1) / seq_along(sigma2_1),type='l',col="blue")
+
+
+
+#### Pop 2 ####
+# theta0
+maximization_2 = optim(function(x) - log_posterior(x,n2,s2,p2),
+                       hessian=T, method = "L-BFGS-B", par=c(1,1))
+theta0_2 = maximization_2$par
+theta0_2
+
+# tau
+tau_2 = 0.8*diag(solve(maximization_2$hessian))
+
+set.seed(1)
+posterior_sample_2 = MH(S = S, B = B, theta0 = theta0_2, 
+                        n = n2, s = s2,
+                        p = p2, tau = tau_2, thin = 3)
+
+plot(posterior_sample_2$values)
+
+posterior_sample_2$acc_rate
+
+alpha_2  = exp(posterior_sample_2$values[,1])
+beta_2   = exp(posterior_sample_2$values[,2])
+
+x11()
+par(mfrow=c(1,2))
+plot(alpha_2, type="l")
+plot(beta_2, type="l")
+
+x11()
+means=c(mean(alpha_2),mean(beta_2))
+means
+medians=c(median(alpha_2),median(beta_2))
+medians
+
+par(mfrow=c(1,2))
+{hist(alpha_2,3000, freq=F,xlab="alpha_2",main="a) Histogram of alpha_2")
+  abline(v=means[1], col="red",lwd=2)
+  abline(v=medians[1], col="green",lwd=2,lty=3)}
+{hist(beta_2,3000, freq=F,xlab="beta_2",main="b) beta_2") 
+  abline(v=means[2], col="red",lwd=2)
+  abline(v=medians[2], col="green",lwd=2,lty=3)}
+
+x11()
+par(mfrow=c(1,2))
+acf(alpha_2,main="ACF for alpha_2",lag.max = 150)
+acf(beta_2,main="ACF for beta_2",lag.max = 150)
+
+mu_2     = alpha_2 / beta_2
+sigma2_2 = alpha_2 / beta_2^2
+
+x11()
+plot(cumsum(mu_2) / seq_along(mu_2),type='l',col="red")
+plot(cumsum(sigma2_2) / seq_along(sigma2_2),type='l',col="blue")
+
+N = length(sigma2_1)
+I = sum(sigma2_1<sigma2_2)/N
+I
+
+delta_H = 1-2*(1-I)
+delta_H
+
+
